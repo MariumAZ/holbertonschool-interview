71c0a3ce6f4f59d9981a1dcc392f03969af671d2
MariumAZ <mariem.azouz@dauphine.tn>
2021-03-09 15:56:04 +0100

    test

--- 0-lockboxes.py
+++ 0-lockboxes.py
@@ -2,7 +2,12 @@
 
 """ first locking boxes"""
 def canUnlockAll(boxes):
-
+      """Determines if all the boxes can be opened or not
+    Args:
+        boxes (list[list]): boxes is the list of box that contains keys
+    Returns:
+        (bool): True if we can open all the boxes, otherwise return False
+    """
     final = [0]
     for i, box in enumerate(boxes):
         for key in box:
